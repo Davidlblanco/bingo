ea61cab80c575d9be09b9d9e86a764bf26b18a66
David Blanco <david.lucas@maeztra.com>
2024-05-20 20:08:06 -0300

    fake api get post patch

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,19 +1,45 @@
+
 type game = {
-    id: 'string',
+    id: number,
     numbers: number[]
 }
-const games: game[] = []
+let games: game[] = []
 
 export function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const id = searchParams.get('id')
     if (!id) return Response.json({ games })
-    return Response.json({ id })
+    try {
+        const game = games.filter(item => item.id === parseInt(id))
+        return Response.json(game[0])
+    }
+    catch (e) {
+        return Response.json({ message: e })
+    }
 }
 
 
-export async function POST(request: Request) {
-    const body = await request.json()
-    console.log(body)
-    return Response.json(body)
+export async function POST() {
+    try {
+        const id = games.length + 1
+        games.push({ id, numbers: [] })
+        return Response.json({ id, message: "Game succesfully created!" })
+    } catch (e) {
+        return Response.json({ message: e })
+    }
+}
+
+
+export async function PATCH(request: Request) {
+    try {
+        const body: game = await request.json()
+        games.forEach(game => {
+            if (game.id === body.id) {
+                game.numbers = body.numbers
+            }
+        })
+        return Response.json({ currentGame: body, message: "Game succesfully modified!" })
+    } catch (e) {
+        return Response.json({ message: e })
+    }
 }
