f90d4956b016ff9d81aa0e6fc81c72717c51c315
David Blanco <david.lucas@maeztra.com>
2024-05-20 20:58:40 -0300

    header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import styles from "./header.module.css";
 export default function Header() {
   
   return (
-    <div>
-        <a href='/sort'>sort</a>
-        <a href='/'>table</a>
+    <div className={styles.header}>
+        <a href='/sort'>Sortear</a>
+        <a href='/'>Tabela</a>
     </div>
   )
 }
